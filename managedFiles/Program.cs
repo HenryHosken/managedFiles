08949609e5c835919e1bb9d64fe25d0af9826762
henry hosken <henry_hosken12@outlook.com>
2023-05-06 12:05:07 -0300

    Adicionar arquivos de projeto.

---
+++ managedFiles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+namespace managedFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\tempPast";
+            
+            StreamReader sr = null;
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("files:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("An error acurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();                
+            }
+        }
+    }
+}
